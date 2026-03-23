221769508baa2357d72953f2349346dd68354c2c
Felipe Lundgren <felipec5l2@hotmail.com>
2026-03-23 15:26:10 -0300

    feat: finalizando backend

--- Data/DataStorage.cs
+++ Data/DataStorage.cs
@@ -4,22 +4,26 @@ namespace DesafioEstagio.Data
 {
     public static class DataStorage
     {
-        // 1. Lista de Colaboradores (com nossos dados de teste)
         public static List<Colaborador> Colaboradores { get; set; } = new List<Colaborador>
         {
             new Colaborador { Id = 1, Nome = "Felipe (Futuro Estagiário)" },
             new Colaborador { Id = 2, Nome = "João da Silva" }
         };
 
-        // 2. Lista de Workshops
-        public static List<Workshop> Workshops { get; set; } = new List<Workshop>();
+        // Adicionamos 1 Workshop de teste
+        public static List<Workshop> Workshops { get; set; } = new List<Workshop>
+        {
+            new Workshop { Id = 1, Nome = "Introdução ao C#", DataRealizacao = DateTime.Now, Descricao = "Aulão para os estagiários" }
+        };
 
-        // 3. Lista de Atas (O QUE ESTAVA FALTANDO PARA O ERRO SUMIR!)
-        public static List<AtaPresenca> Atas { get; set; } = new List<AtaPresenca>();
+        // Adicionamos 1 Ata de teste: O Colaborador 1 (Felipe) participou do Workshop 1 (C#)
+        public static List<AtaPresenca> Atas { get; set; } = new List<AtaPresenca>
+        {
+            new AtaPresenca { Id = 1, WorkshopId = 1, ColaboradorId = 1 }
+        };
 
-        // 4. Contadores para simular os IDs automáticos
         public static int ProximoIdColaborador = 3;
-        public static int ProximoIdWorkshop = 1;
-        public static int ProximoIdAta = 1; // Esse também estava faltando!
+        public static int ProximoIdWorkshop = 2;
+        public static int ProximoIdAta = 2;
     }
 }
