52c36f03943db28fede37cc797ad22be8321055a
Felipe Lundgren <felipec5l2@hotmail.com>
2026-03-23 15:23:35 -0300

    fix: fixing DataStorage

--- Data/DataStorage.cs
+++ Data/DataStorage.cs
@@ -4,17 +4,22 @@ namespace DesafioEstagio.Data
 {
     public static class DataStorage
     {
-        // Já iniciamos a lista com 2 colaboradores cadastrados!
+        // 1. Lista de Colaboradores (com nossos dados de teste)
         public static List<Colaborador> Colaboradores { get; set; } = new List<Colaborador>
         {
             new Colaborador { Id = 1, Nome = "Felipe (Futuro Estagiário)" },
             new Colaborador { Id = 2, Nome = "João da Silva" }
         };
 
+        // 2. Lista de Workshops
         public static List<Workshop> Workshops { get; set; } = new List<Workshop>();
 
-        // Como já usamos o 1 e o 2, o próximo será o 3
+        // 3. Lista de Atas (O QUE ESTAVA FALTANDO PARA O ERRO SUMIR!)
+        public static List<AtaPresenca> Atas { get; set; } = new List<AtaPresenca>();
+
+        // 4. Contadores para simular os IDs automáticos
         public static int ProximoIdColaborador = 3;
         public static int ProximoIdWorkshop = 1;
+        public static int ProximoIdAta = 1; // Esse também estava faltando!
     }
 }
