7b8cebfaa517164ced03e7f6475dfb249a3da0af
Felipe Lundgren <felipec5l2@hotmail.com>
2026-03-23 15:49:22 -0300

    feat: criando rotas e visual

--- fast-frontend/src/app/app.routes.ts
+++ fast-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Colaboradores } from './components/colaboradores/colaboradores';
+import { Workshops } from './components/workshops/workshops';
+import { WorkshopDetalhe } from './components/workshop-detalhe/workshop-detalhe';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/workshops', pathMatch: 'full' },
+  { path: 'colaboradores', component: Colaboradores },
+  { path: 'workshops', component: Workshops },
+  { path: 'workshops/:id', component: WorkshopDetalhe }
+];
